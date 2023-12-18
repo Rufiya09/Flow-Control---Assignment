05895b9ddd4896935a6f946d4da9c944a3953a74
Shaik Rufiya <150911765+Rufiya09@users.noreply.github.com>
2023-12-18 16:23:20 +0530

    4) Write a program in Java to display the first 10 natural numbers

---
+++ Naturalnumbers.java
@@ -0,0 +1,13 @@
+// 	Write a program in Java to display the first 10 natural numbers
+
+public class Naturalnumbers
+{
+	public static void main(String args[])
+	{
+	
+		for(int i = 1; i<=10 ; i++)
+		{
+		System.out.println(i);
+		}
+	}
+}	
